7a6edfd61ad86a6033c3c249d5cdd323b1df50b9
Nguyen <Nguyen>
2021-10-31 13:12:13 +0700

    2332

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http=require("https");
 const app =require('./app');
 const port=process.env.port||3000
-server.listen(port,()=>{
+app.listen(port,()=>{
   console.log("Turn on server");
 })
 

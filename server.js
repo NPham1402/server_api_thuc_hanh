d1dea2681886ac1324d089262e7dbb910cc9c105
Nguyen <Nguyen>
2021-10-31 19:22:57 +0700

    2332

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const getalldata2 =async (req, res, next) => {
   const snapshot=await users.get();
   const array=[]
   if(snapshot.empty)
-  {res.status(404).send({message:"Khong co du lieu"})}
+  {res.status(404).send({message:"Khong co d lieu"})}
   else{
   snapshot.forEach(doc => {
     var element={}
     element.id=doc.id
-    element.name=doc.data().name
-    element.img=doc.data().img
+    element.
     element.arraylist=doc.data().arraylist
   //  console.log(element)
    array.push(element)
@@ -27,16 +26,21 @@ const getalldata2 =async (req, res, next) => {
 const getalldata =async (req, res, next) => {
   const users= await db.collection('Furniture')
   const snapshot=await users.get();
+  const array1=[5]
   const array=[]
   if(snapshot.empty)
   {res.status(404).send({message:"Khong co du lieu"})}
   else{
   snapshot.forEach(doc => {
+    var element2=[]
+    element2.name=doc.data().name
+    element2.img=doc.data().img
+    element2.description=doc.data().description
     var element={}
+    element.id=[]
     element.id=doc.id
-    element.name=doc.data().name
-    element.img=doc.data().img
-    element.description=doc.data().description
+    element.chitiet=[]
+    element.chitiet.push([element2.name,element2.description,element2.img])
   //  console.log(element)
    array.push(element)
   })
